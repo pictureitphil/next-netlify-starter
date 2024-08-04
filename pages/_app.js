c6ac5f31251c96a3f24db0b86b358bd24ed1eeac
Philip Lagud <68571709+pictureitphil@users.noreply.github.com>
2024-08-04 19:27:57 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '@styles/globals.css'
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const WorldMap = () => {
+  const [markers, setMarkers] = useState([]);
 
-export default Application
+  const addMarker = (e) => {
+    const newMarker = {
+      position: e.latlng,
+      name: 'New Location',
+      description: 'Enter description here'
+    };
+    setMarkers([...markers, newMarker]);
+  };
+
+  return (
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100vh', width: '100%' }}
+      onClick={addMarker}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {markers.map((marker, index) => (
+        <Marker key={index} position={marker.position}>
+          <Popup>
+            <h3>{marker.name}</h3>
+            <p>{marker.description}</p>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
+
+export default WorldMap;
