bc297181033be8e2b1c89afa700dadd94bc4110f
Philip Lagud <68571709+pictureitphil@users.noreply.github.com>
2024-08-04 19:31:01 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,37 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-export default function Home() {
+const WorldMap = () => {
+  const [markers, setMarkers] = useState([]);
+
+  const addMarker = (e) => {
+    const newMarker = {
+      position: e.latlng,
+      name: 'New Location',
+      description: 'Enter description here'
+    };
+    setMarkers([...markers, newMarker]);
+  };
+
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100vh', width: '100%' }}
+      onClick={addMarker}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {markers.map((marker, index) => (
+        <Marker key={index} position={marker.position}>
+          <Popup>
+            <h3>{marker.name}</h3>
+            <p>{marker.description}</p>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+export default WorldMap;
